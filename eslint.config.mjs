8d734ba36da9112f222c5a5806d65a8b64c11353
Ariel Jonnathan Camacho Silvestre <ariel.jhonnathan@gmail.com>
2025-10-27 21:19:41 -0400

    chore: add eslint configuration file.

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }]
+    }
+  },
+  {
+    ignores: ['node_modules/', 'dist/', 'coverage/', '*.config.*']
+  }
+];
